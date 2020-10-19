4261b0d9679fcee288a2eabbfae14444485d6496
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-19 10:16:54 -0600

    Add concepts for total results hit

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,6 @@
 class FoodsController < ApplicationController
   def index
     @foods = FoodFacade.foods_by_keyword(params[:q])
+    # @total_results = FoodFacade.keyword_total_results(params[:q])
   end
 end
