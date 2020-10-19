e92b6526d5e0b95c8860343ef1e3bf7225063c6d
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-19 10:16:42 -0600

    Update feature spec for use of vcr

--- spec/features/user_can_search_by_keywords_spec.rb
+++ spec/features/user_can_search_by_keywords_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature "user can search for food" do
-  scenario "user submits a keywords from root page" do
+  scenario "user submits a keywords from root page", :vcr do
     visit '/'
     fill_in :q, with: "sweet potatoes"
     click_on "Search"
